bd540f86cc918d50db2a557c8af8c8ed4dfa59d9
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-02-01 20:35:05 +0530

    fixed

--- src/app/api/scrap/route.ts
+++ src/app/api/scrap/route.ts
@@ -7,10 +7,12 @@ export const POST = async (req: NextRequest) => {
     console.log("Scrap url details :-",data);
     if (data && data.url !== '') {
         try {
+            console.log("Backend Test 1")
             const response = await getMetaData(data.url);
             console.log("Scrap Response :-", response);
             return NextResponse.json({ status: 200, message: response });
         } catch (error) {
+            console.log("Backend Test 2")
             return NextResponse.json({ status: 400, message: error });
         }
     } else {
