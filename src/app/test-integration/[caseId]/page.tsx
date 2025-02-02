e175d29f949d3a028018f5f58a2380cbaf0b5ba5
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-02-02 21:02:21 +0530

    fixed

--- src/app/test-integration/[caseId]/page.tsx
+++ src/app/test-integration/[caseId]/page.tsx
@@ -1,23 +1,27 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import TestIntegration from '../../../../components/TestIntegration/TestIntegration';
 type PageProps = {
     params: Promise<{ caseId: string }>;
 };
-function IntegrationCase({
-    params,
-}: PageProps) {
-    const [caseId, setCaseId] = React.useState<string | null>(null);
+function IntegrationCase({params}: PageProps) {
+    const [caseId, setCaseId] = useState<string | null>(null);
 
-    if (!caseId) {
-        return <div>Loading...</div>;
-    } else {
-        return (
-            <div>
-                <TestIntegration caseId={caseId} />
-            </div>
-        )
-    }
+    useEffect(() => {
+      async function fetchParams() {
+        const resolvedParams = await params;
+        setCaseId(resolvedParams.caseId);
+      }
+  
+      fetchParams();
+    }, [params]);
+  
+    return (
+      <div>
+        {caseId ? <TestIntegration caseId={caseId} /> : <p>Loading...</p>}
+      </div>
+    );
+
 }
 
 export default IntegrationCase
